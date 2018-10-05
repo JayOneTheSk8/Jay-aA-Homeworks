d8ce65efa6d809801c5bb629881be4501388402e
Justin <misterjustincox@gmail.com>
2018-10-05 07:24:39 -0400

    Add to cache correctly

--- w2d5/lru_cache.rb
+++ w2d5/lru_cache.rb
@@ -11,13 +11,15 @@ class LRUCache
   end
 
   def add(el)
-    # if self.cache.include?(el)
-    #   self.cache.delete(el)
-    #   self.cache << el
-    # elsif count >= self.size
-    #   self.cache.shift
-    #
-    # end
+    if self.cache.include?(el)
+      self.cache.delete(el)
+      self.cache << el
+    elsif count >= self.size
+      self.cache.shift
+      self.cache << el
+    else
+      self.cache << el
+    end
   end
 
   def show
