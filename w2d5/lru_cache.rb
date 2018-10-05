7e1f41d038af53086e8e29b2c0e4001b08111896
Justin <misterjustincox@gmail.com>
2018-10-05 07:29:48 -0400

    Finish building cache based on LRU Principle

--- w2d5/lru_cache.rb
+++ w2d5/lru_cache.rb
@@ -23,6 +23,24 @@ class LRUCache
   end
 
   def show
-
+    p self.cache
   end
 end
+
+johnny_cache = LRUCache.new(4)
+
+johnny_cache.add("I walk the line")
+johnny_cache.add(5)
+
+johnny_cache.count == 2
+johnny_cache.add([1,2,3])
+johnny_cache.add(5)
+johnny_cache.add(-5)
+johnny_cache.add({a: 1, b: 2, c: 3})
+johnny_cache.add([1,2,3,4])
+johnny_cache.add("I walk the line")
+johnny_cache.add(:ring_of_fire)
+johnny_cache.add("I walk the line")
+johnny_cache.add({a: 1, b: 2, c: 3})
+
+johnny_cache.show
