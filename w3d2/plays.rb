89e3465bf65854c64e01812be3cbfbac4c46560e
Justin <misterjustincox@gmail.com>
2018-10-09 06:14:54 -0400

    Finish ORM

--- w3d2/plays.rb
+++ w3d2/plays.rb
@@ -19,6 +19,30 @@ class Play
     data.map { |datum| Play.new(datum) }
   end
 
+  def self.find_by_title(title)
+    PlayDBConnection.instance.execute(<<-SQL, title)
+      SELECT
+        title
+      FROM
+        plays
+      WHERE
+        title = ?
+    SQL
+  end
+
+  def self.find_by_playwright(name)
+    PlayDBConnection.instance.execute(<<-SQL, name)
+      SELECT
+        title
+      FROM
+        plays
+      JOIN
+        playwrights ON plays.playwright_id = playwrights.id
+      WHERE
+        name = ?
+    SQL
+  end
+
   def initialize(options)
     @id = options['id']
     @title = options['title']
@@ -48,4 +72,65 @@ class Play
         id = ?
     SQL
   end
+end
+
+class Playwright
+  attr_accessor :id, :name, :birth_year
+
+  def self.all
+    data = PlayDBConnection.instance.execute('SELECT * FROM playwrights')
+    data.map { |datum| Playwright.new(datum) }
+  end
+
+  def self.find_by_name(name)
+    PlayDBConnection.instance.execute(<<-SQL, name)
+      SELECT
+        title
+      FROM
+        playwrights
+      JOIN
+        plays ON plays.playwright_id = playwrights.id
+      WHERE
+        name = ?
+    SQL
+  end
+
+  def initialize(options)
+    @id, @name, @birth_year = options.values_at('id', 'name', 'birth_year')
+  end
+
+  def create
+    raise "#{self} already exists" if @id
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year)
+      INSERT INTO
+        playwrights (name, birth_year)
+      VALUES
+        (?, ?)
+    SQL
+    @id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} does not exist yet" unless @id
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year, @id)
+      UPDATE
+        playwrights
+      SET
+        name = ?, birth_year = ?
+      WHERE
+        id = ?
+    SQL
+  end
+
+  def get_plays
+    PlayDBConnection.instance.execute(<<-SQL, @id)
+      SELECT
+        title
+      FROM
+        plays
+      WHERE
+        playwright_id = ?
+    SQL
+  end
+
 end
